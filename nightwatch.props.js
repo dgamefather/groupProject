1c9c4be9b9ef7551d46c9ecc03689710487a7ea4
dgamefather <dgamefather@thatoneplace.net>
2019-12-02 09:35:18 -0700

    Got conf working for Mac and PC

--- nightwatch.props.js
+++ nightwatch.props.js
@@ -1,5 +1,13 @@
 module.exports = {
+
+    // PC
     resourcePath: "..\\testing-resources\\",
     seleniumServer: "selenium-server-standalone-3.141.59.jar",
-    chromedriver: "chromedriver.exe"
+    chromedriver: "chromedriver.exe",
+
+    // // Mac
+    // resourcePath: "../testing-resources/",
+    // seleniumServer: "selenium-server-standalone-3.141.59.jar",
+    // chromedriver: "chromedriver"
+
  }
